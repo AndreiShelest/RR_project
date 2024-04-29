8282ce6c22a8061c21b8efa3ceea0b8fae61d48b
Andrei Shelest <shelest.andrej.12@gmail.com>
2024-04-29 15:29:10 +0200

    add scaling

--- src/modelling.py
+++ src/modelling.py
@@ -1,4 +1,6 @@
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 import pandas as pd
 import json
 import numpy as np
@@ -9,14 +11,29 @@ def main():
         config = json.load(config_file)
 
     tickers_train_path = config['data']['train_path']
+    interim_train_path = config['data']['interim_train_path']
     tickers = config['tickers']
+
+    pipelines = {}
 
     for ticker in tickers:
         train_df = pd.read_csv(f'{tickers_train_path}/{ticker}.csv', index_col='date')
 
         ind_imputer = imputation.IndicatorImputer()
-        ind_imputer.fit(train_df)
-        imputed_df = ind_imputer.transform(train_df)
+
+        pipelines[ticker] = Pipeline(
+            [
+                ('imputer', ind_imputer),
+                ('normalizer', MinMaxScaler()),
+            ]
+        )
+
+        transformed = pipelines[ticker].fit_transform(train_df)
+        transformed_df = pd.DataFrame(data=transformed, columns=train_df.columns)
+        transformed_df.insert(0, train_df.index.name, train_df.index)
+        transformed_df.set_index(train_df.index.name, inplace=True)
+
+        transformed_df.to_csv(f'{interim_train_path}/{ticker}.csv')
 
 
 if __name__ == '__main__':
