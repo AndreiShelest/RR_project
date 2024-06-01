8d2b77c6336a86ec56d5f71c2be61c9168cda31d
Andrei Shelest <shelest.andrej.12@gmail.com>
2024-06-01 13:48:40 +0200

    persist strategy stats

--- src/strategy_execution.py
+++ src/strategy_execution.py
@@ -2,6 +2,7 @@ from backtesting import Strategy, Backtest
 import json
 import pandas as pd
 from system_types import system_types
+from pathlib import Path
 
 
 class TradingStrategy(Strategy):
@@ -22,12 +23,37 @@ class TradingStrategy(Strategy):
         self.prev_signal = curr_signal
 
 
+_return_label = "Return"
+_return_pa_label = "Return (p.a.)"
+_vol_pa_label = "Vol (p.a.)"
+_bh_return_label = "B&H Return"
+_sharpe_label = "Sharpe Ratio"
+_mdd_label = "Max Drawdown"
+_accuracy_label = "Accuracy"
+_transactions_label = "Transactions"
+
+_strategy_metrics = [
+    ("Return [%]", _return_label),
+    ("Return (Ann.) [%]", _return_pa_label),
+    ("Volatility (Ann.) [%]", _vol_pa_label),
+    ("Buy & Hold Return [%]", _bh_return_label),
+    ("Sharpe Ratio", _sharpe_label),
+    ("Max. Drawdown [%]", _mdd_label),
+    ("Win Rate [%]", _accuracy_label),
+    ("# Trades", _transactions_label),
+]
+_strategy_metrics_labels = [label_pair[1] for label_pair in _strategy_metrics]
+
+
 def main():
     with open('./project_config.json', 'r') as config_file:
         config = json.load(config_file)
     tickers_test_path = config['data']['test_path']
     signal_path = config['data']['signal_path']
+    basic_stats_path = config['data']['basic_stats_path']
     tickers = config['tickers']
+
+    basic_stats_data = {}
 
     for ticker in tickers:
         test_df = pd.read_csv(f'{tickers_test_path}/{ticker}.csv', index_col='Date')
@@ -49,9 +75,22 @@ def main():
                 data=joined_df, strategy=TradingStrategy, cash=100000, commission=0
             )
             results = backtest.run()
+
+            results.rename(dict(_strategy_metrics), inplace=True)
+
+            if basic_stats_data.get(system_type) is None:
+                basic_stats_data[system_type] = pd.DataFrame(
+                    index=_strategy_metrics_labels
+                )
+                basic_stats_data[system_type].index.name = 'Metric'
+            basic_stats_data[system_type][ticker] = results
+
             print(
-                f'[{system_type}] Ticker={ticker}, Strategy Return={results["Return [%]"]}, B&H={results["Buy & Hold Return [%]"]}'
+                f'[{system_type}] Ticker={ticker}, Strategy Return={results[_return_label]}, B&H={results[_bh_return_label]}'
             )
+
+    for system_type in basic_stats_data:
+        basic_stats_data[system_type].to_csv(f'{basic_stats_path}/{system_type}.csv')
 
     return
 
