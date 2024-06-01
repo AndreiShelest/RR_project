6c633c9cef2f329f084c4b0ff351199a36778f2e
Andrei Shelest <32816003+AndreiShelest@users.noreply.github.com>
2024-06-01 23:41:57 +0200

    Plotting (#13)
    * compute equity
    
    * alter strategy
    
    * more plotting

--- src/strategies.py
+++ src/strategies.py
@@ -8,15 +8,21 @@ class TradingStrategy(Strategy):
 
         if self.data[signal_label][0] == 1:
             self.buy()
+        elif self.data[signal_label][0] == 0:
+            self.sell()
 
     def next(self):
         curr_signal = self.data[signal_label][-1]
         prev_signal = self.data[signal_label][-2]
 
-        if curr_signal == 1 and prev_signal == 0 and not self.position:
+        if curr_signal == 1 and prev_signal == 0:
+            if self.position:
+                self.position.close()
             self.buy()
-        elif curr_signal == 0 and prev_signal == 1 and self.position:
-            self.position.close()
+        elif curr_signal == 0 and prev_signal == 1:
+            if self.position:
+                self.position.close()
+            self.sell()
 
 
 class BuyAndHoldStrategy(Strategy):
--- src/strategy_execution.py
+++ src/strategy_execution.py
@@ -4,54 +4,40 @@ import pandas as pd
 from system_types import system_types
 from pathlib import Path
 import numpy as np
-from constants import date_index_label, signal_label, buy_hold_system
+from constants import (
+    date_index_label,
+    signal_label,
+    buy_hold_system,
+    equity_label,
+    equity_curve_label,
+    strategy_metrics,
+    strategy_metrics_labels,
+    return_label,
+    bh_return_label,
+)
 from strategies import TradingStrategy, BuyAndHoldStrategy
-
-
-_return_label = "Return"
-_return_pa_label = "Return (p.a.)"
-_vol_pa_label = "Vol (p.a.)"
-_bh_return_label = "B&H Return"
-_sharpe_label = "Sharpe Ratio"
-_mdd_label = "Max Drawdown"
-_accuracy_label = "Accuracy"
-_transactions_label = "Positions Taken"
-
-_strategy_metrics = [
-    ("Return [%]", _return_label),
-    ("Return (Ann.) [%]", _return_pa_label),
-    ("Volatility (Ann.) [%]", _vol_pa_label),
-    ("Buy & Hold Return [%]", _bh_return_label),
-    ("Sharpe Ratio", _sharpe_label),
-    ("Max. Drawdown [%]", _mdd_label),
-    ("Win Rate [%]", _accuracy_label),
-    ("# Trades", _transactions_label),
-]
-_strategy_metrics_labels = [label_pair[1] for label_pair in _strategy_metrics]
 
 
 def _validate_amount_of_trades(signal: pd.Series, trades_actual):
     shifted_signal = np.pad(
         signal, pad_width=(0, 1), constant_values=(0, signal.iat[-1])
     )[1:]
-    trades_required = (np.abs(shifted_signal - signal) > 0).sum() // 2 + (
-        1 if signal.iat[0] == 1 else 0
-    )
+    trades_required = (np.abs(shifted_signal - signal) > 0).sum() + 1
     print(
         f'Trades required={trades_required}, trades actual={trades_actual}, valid={trades_required == trades_actual}'
     )
 
 
-def _run_bh_strategy(test_df):
+def _run_bh_strategy(test_df, initial_cash):
     backtest = Backtest(
         data=test_df,
         strategy=BuyAndHoldStrategy,
-        cash=100000,
+        cash=initial_cash,
         commission=0,
         trade_on_close=True,
     )
     results = backtest.run()
-    results.rename(dict(_strategy_metrics), inplace=True)
+    results.rename(dict(strategy_metrics), inplace=True)
 
     return results
 
@@ -62,15 +48,27 @@ def main():
     tickers_test_path = config['data']['test_path']
     signal_path = config['data']['signal_path']
     basic_stats_path = config['data']['basic_stats_path']
+    ts_stats_path = config['data']['time_series_stats_path']
     tickers = config['tickers']
+    initial_cash = config['strategy']['initial_cash']
 
     basic_stats_data = {}
+    time_series_data = {}
 
-    def assign_basis_stats(system_type, results):
+    def assign_basic_stats(system_type, ticker, results):
         if basic_stats_data.get(system_type) is None:
-            basic_stats_data[system_type] = pd.DataFrame(index=_strategy_metrics_labels)
+            basic_stats_data[system_type] = pd.DataFrame(index=strategy_metrics_labels)
             basic_stats_data[system_type].index.name = 'Metric'
         basic_stats_data[system_type][ticker] = results
+
+    def assign_ts_stats(system_type, ticker, results):
+        if time_series_data.get(equity_label) is None:
+            time_series_data[equity_label] = {}
+        if time_series_data[equity_label].get(ticker) is None:
+            time_series_data[equity_label][ticker] = pd.DataFrame()
+        time_series_data[equity_label][ticker][system_type] = results[
+            equity_curve_label
+        ][equity_label]
 
     for ticker in tickers:
         test_df = pd.read_csv(
@@ -81,8 +79,9 @@ def main():
         test_df['High'] = test_df['Close']
         test_df['Low'] = test_df['Close']
 
-        bh_results = _run_bh_strategy(test_df)
-        assign_basis_stats(buy_hold_system, bh_results)
+        bh_results = _run_bh_strategy(test_df, initial_cash)
+        assign_basic_stats(buy_hold_system, ticker, bh_results)
+        assign_ts_stats(buy_hold_system, ticker, bh_results)
 
         for system_type in system_types:
             signal_df = pd.read_csv(
@@ -98,23 +97,32 @@ def main():
             backtest = Backtest(
                 data=joined_df,
                 strategy=TradingStrategy,
-                cash=100000,
+                cash=initial_cash,
                 commission=0,
                 trade_on_close=True,
             )
             results = backtest.run()
-            results.rename(dict(_strategy_metrics), inplace=True)
+            results.rename(dict(strategy_metrics), inplace=True)
 
-            assign_basis_stats(system_type, results)
+            assign_basic_stats(system_type, ticker, results)
+            assign_ts_stats(system_type, ticker, results)
 
             print(
-                f'[{system_type}] Ticker={ticker}, Strategy Return={results[_return_label]}, B&H={results[_bh_return_label]}'
+                f'[{system_type}] Ticker={ticker}, Strategy Return={results[return_label]}, B&H={results[bh_return_label]}'
             )
 
             _validate_amount_of_trades(signal_df[signal_label], len(results['_trades']))
 
     for system_type in basic_stats_data:
         basic_stats_data[system_type].to_csv(f'{basic_stats_path}/{system_type}.csv')
+
+    for ts_label in time_series_data:
+        Path(f'{ts_stats_path}/{ts_label}').mkdir(exist_ok=True)
+
+        for ticker in time_series_data[ts_label]:
+            time_series_data[ts_label][ticker].to_csv(
+                f'{ts_stats_path}/{ts_label}/{ticker}.csv'
+            )
 
     return
 
