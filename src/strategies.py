84bc737ffc3c0f6e0ee5684db66af3c29ab50e70
Andrei Shelest <shelest.andrej.12@gmail.com>
2024-06-01 22:02:42 +0200

    alter strategy

--- src/strategies.py
+++ src/strategies.py
@@ -13,10 +13,14 @@ class TradingStrategy(Strategy):
         curr_signal = self.data[signal_label][-1]
         prev_signal = self.data[signal_label][-2]
 
-        if curr_signal == 1 and prev_signal == 0 and not self.position:
+        if curr_signal == 1 and prev_signal == 0:
+            if self.position:
+                self.position.close()
             self.buy()
-        elif curr_signal == 0 and prev_signal == 1 and self.position:
-            self.position.close()
+        elif curr_signal == 0 and prev_signal == 1:
+            if self.position:
+                self.position.close()
+            self.sell()
 
 
 class BuyAndHoldStrategy(Strategy):
