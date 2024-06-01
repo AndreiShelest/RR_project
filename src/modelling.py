0c75a1c4c05f97b4663b739af6caadddce36f66e
Andrei Shelest <shelest.andrej.12@gmail.com>
2024-06-01 11:20:50 +0200

    add several system types

--- src/modelling.py
+++ src/modelling.py
@@ -5,6 +5,7 @@ import pandas as pd
 import json
 from xgboost import XGBClassifier
 from sklearn.base import TransformerMixin, BaseEstimator
+from pathlib import Path
 
 
 class Debug(BaseEstimator, TransformerMixin):
@@ -30,20 +31,58 @@ class Debug(BaseEstimator, TransformerMixin):
         return self
 
 
+def _generate_test_signal(
+    config,
+    ticker,
+    X_train: pd.DataFrame,
+    Y_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    Y_test: pd.DataFrame,
+):
+    signal_path = config['data']['signal_path']
+    pca_components = config['modelling']['pca']['components']
+    # interim_train_path = config['data']['interim_train_path']
+
+    xgboost_settings = config['modelling']['xgboost']
+
+    pipeline_steps_map = {
+        'without_pca': [
+            ('normalizer', MinMaxScaler()),
+            ('xgboost', XGBClassifier(**xgboost_settings)),
+        ],
+        'with_pca': [
+            ('normalizer', MinMaxScaler()),
+            ('pca', PCA(n_components=pca_components)),
+            ('xgboost', XGBClassifier(**xgboost_settings)),
+        ],
+    }
+
+    for system_type in pipeline_steps_map:
+        pipeline_steps = pipeline_steps_map[system_type]
+
+        model = Pipeline(pipeline_steps)
+        model.fit(X_train, Y_train)
+
+        test_score = model.score(X_test, Y_test)
+        print(f'System={system_type}, ticker={ticker}, test_score={test_score}')
+
+        signal = model.predict(X_test)
+
+        signal_df = pd.DataFrame({'Signal': signal}, index=X_test.index)
+
+        Path(f'{signal_path}/{system_type}').mkdir(exist_ok=True)
+        signal_df.to_csv(f'{signal_path}/{system_type}/{ticker}.csv')
+
+
 def main():
     with open('./project_config.json', 'r') as config_file:
         config = json.load(config_file)
 
     tickers_train_path = config['data']['train_path']
     tickers_test_path = config['data']['test_path']
-    interim_train_path = config['data']['interim_train_path']
-    signal_path = config['data']['signal_path']
+
     target_feature_path = config['data']['target_var_path']
     tickers = config['tickers']
-
-    pca_components = config['modelling']['pca']['components']
-
-    pipelines = {}
 
     for ticker in tickers:
         train_df = pd.read_csv(f'{tickers_train_path}/{ticker}.csv', index_col='Date')
@@ -53,35 +92,14 @@ def main():
             f'{target_feature_path}/{ticker}.csv', index_col='Date'
         ).loc[train_df.index]
 
-        pca = PCA(n_components=pca_components)
-        bst = XGBClassifier(
-            booster='gbtree',
-            n_estimators=100,
-            objective='binary:logistic',
-            eval_metric='auc',
-        )
-
-        pipeline_steps = [
-            ('normalizer', MinMaxScaler()),
-            ('pca', pca),
-            ('debug', Debug(ticker, train_df.index, interim_train_path)),
-            ('xgboost', bst),
-        ]
-
-        pipelines[ticker] = Pipeline(pipeline_steps)
-        pipelines[ticker].fit(train_df, train_feat_df)
-
         test_df = pd.read_csv(f'{tickers_test_path}/{ticker}.csv', index_col='Date')
         test_feat_df = pd.read_csv(
             f'{target_feature_path}/{ticker}.csv', index_col='Date'
         ).loc[test_df.index]
 
-        test_score = pipelines[ticker].score(test_df, test_feat_df)
-        print(f'{ticker}; test_score={test_score}')
-
-        signal = pipelines[ticker].predict(test_df)
-        signal_df = pd.DataFrame({'Signal': signal}, index=test_df.index)
-        signal_df.to_csv(f'{signal_path}/{ticker}.csv')
+        _generate_test_signal(
+            config, ticker, train_df, train_feat_df, test_df, test_feat_df
+        )
 
 
 if __name__ == '__main__':
