dce3b1c58b3f040d450b3617e6017e14e5b0c3d9
Zofia Bracha <z.bracha@student.uw.edu.pl>
2024-06-02 11:41:35 +0200

    inital setup

--- src/modelling.py
+++ src/modelling.py
@@ -33,7 +33,156 @@ class Debug(BaseEstimator, TransformerMixin):
 
         transformed_df.to_csv(f'{self.interim_path}/{self.ticker}.csv')
         return self
+    
+class Optimizer_xgb(BaseEstimator, TransformerMixin):
+    def __init__(self, n_individuals=128, n_generations=100, patience=10, cxpb=0.5, mutpb=0.2):
+        self.n_individuals = n_individuals
+        self.n_generations = n_generations
+        self.patience = patience
+        self.cxpb = cxpb
+        self.mutpb = mutpb
+        self.best_params_ = None
+    
+    def fit(self, X, y=None):
+        creator.create("FitnessMulti", base.Fitness, weights=(1.0,))
+        creator.create("Individual", list, fitness=creator.FitnessMulti)
+        
+        toolbox = base.Toolbox()
+        toolbox.register("attr_float", random.uniform, 0.01, 1.0)
+        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=5)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+        
+        def evaluate(individual):
+            eta, gamma, max_depth, min_child_weight, subsample = individual
+            params = {
+                'booster': 'gbtree',
+                'objective': 'binary:logistic',
+                'eval_metric': 'auc',
+                'eta': eta,
+                'gamma': gamma,
+                'max_depth': int(max_depth * 10) + 1,
+                'min_child_weight': min_child_weight * 10,
+                'subsample': subsample,
+                'n_estimators': 100
+            }
+            model = xgb.XGBClassifier(**params)
+            scores = cross_val_score(model, X, y, cv=3, scoring='roc_auc')
+            return (np.mean(scores),)
+        
+        toolbox.register("evaluate", evaluate)
+        toolbox.register("mate", tools.cxTwoPoint)
+        toolbox.register("mutate", tools.mutPolynomialBounded, low=0.01, up=1.0, eta=0.1, indpb=0.2)
+        toolbox.register("select", tools.selNSGA2)
+        
+        population = toolbox.population(n=self.n_individuals)
+        hall_of_fame = tools.HallOfFame(1)
+        
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean)
+        stats.register("std", np.std)
+        stats.register("min", np.min)
+        stats.register("max", np.max)
+        
+        class EarlyStopping:
+            def __init__(self, patience=10):
+                self.patience = patience
+                self.counter = 0
+                self.best_score = None
+            
+            def stop(self, score):
+                if self.best_score is None or score > self.best_score:
+                    self.best_score = score
+                    self.counter = 0
+                else:
+                    self.counter += 1
+                return self.counter >= self.patience
+        
+        early_stopping = EarlyStopping(patience=self.patience)
+        
+        for gen in range(self.n_generations):
+            offspring = algorithms.varAnd(population, toolbox, cxpb=self.cxpb, mutpb=self.mutpb)
+            fits = toolbox.map(toolbox.evaluate, offspring)
+            
+            for fit, ind in zip(fits, offspring):
+                ind.fitness.values = fit
+            
+            population = toolbox.select(offspring, len(population))
+            
+            best = tools.selBest(population, 1)[0]
+            if early_stopping.stop(best.fitness.values[0]):
+                print(f"Early stopping at generation {gen}")
+                break
+            
+            if gen in [4, 6, 8]:
+                toolbox.unregister("mutate")
+                if gen == 4:
+                    toolbox.register("mutate", tools.mutPolynomialBounded, low=0.01, up=1.0, eta=0.1, indpb=0.3)
+                elif gen == 6:
+                    toolbox.register("mutate", tools.mutPolynomialBounded, low=0.01, up=1.0, eta=0.1, indpb=0.35)
+                elif gen == 8:
+                    toolbox.register("mutate", tools.mutPolynomialBounded, low=0.01, up=1.0, eta=0.1, indpb=0.4)
+            
+            record = stats.compile(population)
+            print(f"Gen: {gen}, Record: {record}")
+        
+        hall_of_fame.update(population)
+        best_individual = hall_of_fame[0]
+        
+        self.best_params_ = {
+            'eta': best_individual[0],
+            'gamma': best_individual[1],
+            'max_depth': int(best_individual[2] * 10) + 1,
+            'min_child_weight': best_individual[3] * 10,
+            'subsample': best_individual[4],
+            'n_estimators': 100
+        }
+        
+        return self
 
+    def transform(self, X, y=None):
+        return X
+    
+    def fit_transform(self, X, y=None):
+        self.fit(X, y)
+        return self.transform(X, y)
+
+class XGBoostClassifier(BaseEstimator):
+    def __init__(self, booster='gbtree', objective='binary:logistic', eta=0.3, gamma=0, max_depth=6, 
+                 min_child_weight=1, subsample=1, n_estimators=100):
+        self.booster = booster
+        self.objective = objective
+        self.eta = eta
+        self.gamma = gamma
+        self.max_depth = max_depth
+        self.min_child_weight = min_child_weight
+        self.subsample = subsample
+        self.n_estimators = n_estimators
+        self.model = None
+    
+    def fit(self, X, y):
+        self.model = xgb.XGBClassifier(
+            booster=self.booster,
+            objective=self.objective,
+            eta=self.eta,
+            gamma=self.gamma,
+            max_depth=self.max_depth,
+            min_child_weight=self.min_child_weight,
+            subsample=self.subsample,
+            n_estimators=self.n_estimators
+        )
+        self.model.fit(X, y)
+        return self
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
+    
+    def score(self, X, y):
+        y_pred = self.predict_proba(X)[:, 1]
+        return roc_auc_score(y, y_pred)
+    
 class Wavelet(BaseEstimator, TransformerMixin):
 
     def __init__(self, mode, level, wavelet) -> None:
