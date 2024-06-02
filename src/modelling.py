26339e5480b0c06737b4a57ab81f27ecb91dd8e5
Andrei Shelest <shelest.andrej.12@gmail.com>
2024-06-02 21:25:29 +0200

    small refactoring

--- src/modelling.py
+++ src/modelling.py
@@ -3,110 +3,10 @@ from sklearn.decomposition import PCA
 import pandas as pd
 import json
 from xgboost import XGBClassifier
-from sklearn.base import TransformerMixin, BaseEstimator
 from pathlib import Path
 from system_types import create_pipeline, system_types
 from constants import date_index_label, signal_label
-import pywt
-import numpy as np
-from skimage.restoration import denoise_wavelet
-from xgboost_hyperparameters_search import find_xgboost_hyperparameters
-
-
-class Debug(BaseEstimator, TransformerMixin):
-
-    def __init__(self, ticker, df_index, interim_path) -> None:
-        super().__init__()
-        self.ticker = ticker
-        self.df_index = df_index
-        self.interim_path = interim_path
-
-    def transform(self, X):
-        return X
-
-    def fit(self, X, y=None, **fit_params):
-        transformed_df = pd.DataFrame(
-            data=X,
-            columns=[f'C{idx}' for idx, _ in enumerate(X[0])],
-        )
-        transformed_df.insert(0, self.df_index.name, self.df_index)
-        transformed_df.set_index(self.df_index.name, inplace=True)
-
-        transformed_df.to_csv(f'{self.interim_path}/{self.ticker}.csv')
-        return self
-
-
-class Wavelet(BaseEstimator, TransformerMixin):
-
-    def __init__(self, mode, level, wavelet) -> None:
-        # self.ticker = ticker
-        # self.df_index = df_index
-        # self.interim_path = interim_path
-        self.wavelet = wavelet
-        self.mode = mode
-        self.level = level
-
-    def fit(self, X, y=None):
-        # Calculate the thresholds from the training data
-        self.train_data_ = X.copy()
-
-        self.thresholds_ = []
-
-        n_features = X.shape[1]
-
-        for feature_idx in range(n_features):
-            # each feature is done separetely
-            feature_data = X[:, feature_idx]
-            coeffs = pywt.wavedec(feature_data, self.wavelet, level=self.level)
-
-            threshold = self.threshold_coefficients(coeffs)
-            self.thresholds_.append(threshold)
-        return self
-
-    def threshold_coefficients(self, coeffs):
-        # applies thresholding optimisation
-        denoised_coeffs = [coeffs[0]]  # Keep approximation coefficients intact
-        denoised_coeffs += [
-            denoise_wavelet(
-                c,
-                method='BayesShrink',
-                mode=self.mode,
-                wavelet_levels=self.level,
-                wavelet=self.wavelet,
-                rescale_sigma=True,
-            )
-            for c in coeffs[1:]
-        ]
-        return denoised_coeffs
-
-    def transform(self, X, y=None):
-        # incremental denoising
-        n_samples, n_features = X.shape
-        X_denoised = np.zeros_like(X)
-
-        for feature_idx in range(n_features):
-            feature_data = X[:, feature_idx]
-            train_feature_data = self.train_data_[:, feature_idx]
-            denoised_feature_data = np.zeros(n_samples)
-            extended_data = np.concatenate([train_feature_data, feature_data])
-
-            for i in range(n_samples):
-                data_point = extended_data[len(train_feature_data) + i]
-                data_up_to_point = extended_data[: len(train_feature_data) + i + 1]
-
-                coeffs = pywt.wavedec(data_up_to_point, self.wavelet, level=self.level)
-                coeffs_thresholded = self.threshold_coefficients(coeffs)
-                denoised_signal = self.reconstruct_signal(coeffs_thresholded)
-
-                denoised_point = denoised_signal[-1]
-                denoised_feature_data[i] = denoised_point
-
-            X_denoised[:, feature_idx] = denoised_feature_data
-
-        return X_denoised
-
-    def reconstruct_signal(self, coeffs):
-        return pywt.waverec(coeffs, self.wavelet)
+from custom_estimators import Wavelet
 
 
 def _generate_test_signal(
@@ -124,23 +24,6 @@ def _generate_test_signal(
     strategy_settings,
     seed,
 ):
-    transformer = create_pipeline(
-        system_type,
-        normalizer=MinMaxScaler(),
-        pca=PCA(**pca_settings),
-        dwt=Wavelet(**dwt_params),
-    )
-    find_xgboost_hyperparameters(
-        ticker,
-        X_train,
-        Y_train,
-        X_val,
-        Y_val,
-        xgboost_settings,
-        strategy_settings,
-        transformer,
-        seed,
-    )
 
     model = create_pipeline(
         system_type,
